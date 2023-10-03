68ea899f85120f92910b27f9d4fb5cbd31b3a204
Yoandel Obispa <302631801@student.rocmondriaan.nl>
2023-10-03 20:41:00 +0200

    made index.html

---
+++ js/main.js
@@ -0,0 +1,8 @@
+console.log("main.js loaded");
+
+const navbarToggler = document.querySelector(".navbar-toggler")
+navbarToggler.addEventListener("click", navbarChange)
+
+function navbarChange() {
+    document.querySelector(".navbar-toggler-custom-icon").classList.toggle("change");
+}
