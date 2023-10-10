b71bde1c2c992c1a5ed49048cdc0953b6bf44a33
sHoAsjoe <302926554@student.rocmondriaan.nl>
2023-10-10 13:21:26 +0200

    test

---
+++ js/cv.js
@@ -0,0 +1,38 @@
+console.log("hello world");
+onsole.log("cv.js loaded");
+
+const navbarToggler = document.querySelector(".navbar-toggler");
+
+navbarToggler.addEventListener("click", navbarChange);
+
+let loaded = sessionStorage.getItem("loadedKey");
+
+checkLoadingscreen(loaded);
+
+function checkLoadingscreen(loaded) {
+  if (loaded == "true") {
+    document.querySelector(".page").classList.remove("hidden");
+
+    document.querySelector(".loading-screen").classList.add("hidden");
+
+    console.log("You have seen loading screen already");
+  } else {
+    setTimeout(removeDN, 0);
+
+    loaded = true;
+
+    sessionStorage.setItem("loadedKey", loaded);
+  }
+}
+
+function removeDN() {
+  document.querySelector(".page").classList.remove("hidden");
+
+  document.querySelector(".loading-screen").classList.add("hidden");
+}
+
+function navbarChange() {
+  document
+    .querySelector(".navbar-toggler-custom-icon")
+    .classList.toggle("change");
+}
