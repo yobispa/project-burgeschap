80bcf7374d718d8e6735c57856cb696679a7233b
sHoAsjoe <302926554@student.rocmondriaan.nl>
2023-10-11 09:00:32 +0200

    bug opgelost

--- js/cv.js
+++ js/cv.js
@@ -1,38 +1,59 @@
-console.log("hello world");
-onsole.log("cv.js loaded");
+console.log("main.js loaded");
 
-const navbarToggler = document.querySelector(".navbar-toggler");
+ 
 
-navbarToggler.addEventListener("click", navbarChange);
+const navbarToggler = document.querySelector(".navbar-toggler")
 
-let loaded = sessionStorage.getItem("loadedKey");
+navbarToggler.addEventListener("click", navbarChange)
+
+ 
+
+let loaded = sessionStorage.getItem("loadedKey")
 
 checkLoadingscreen(loaded);
 
+ 
+
 function checkLoadingscreen(loaded) {
-  if (loaded == "true") {
-    document.querySelector(".page").classList.remove("hidden");
 
-    document.querySelector(".loading-screen").classList.add("hidden");
+   
 
-    console.log("You have seen loading screen already");
-  } else {
-    setTimeout(removeDN, 0);
+    if(loaded == 'true') {
 
-    loaded = true;
+        document.querySelector(".page").classList.remove("hidden")
 
-    sessionStorage.setItem("loadedKey", loaded);
-  }
+        document.querySelector(".loading-screen").classList.add("hidden")
+
+        console.log("You have seen loading screen already")
+
+    } else {
+
+        setTimeout(removeDN, 3000)
+
+        loaded = true;
+
+        sessionStorage.setItem("loadedKey", loaded)
+
+    }
+
+   
+
 }
+
+ 
 
 function removeDN() {
-  document.querySelector(".page").classList.remove("hidden");
 
-  document.querySelector(".loading-screen").classList.add("hidden");
+    document.querySelector(".page").classList.remove("hidden")
+
+    document.querySelector(".loading-screen").classList.add("hidden")
+
 }
 
+ 
+
 function navbarChange() {
-  document
-    .querySelector(".navbar-toggler-custom-icon")
-    .classList.toggle("change");
+
+    document.querySelector(".navbar-toggler-custom-icon").classList.toggle("change");
+
 }
